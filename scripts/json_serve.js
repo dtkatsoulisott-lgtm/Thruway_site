f3226616647b9c92fe4cbd3ffe67da71064a7aed
landwhich <will.f.richards@gmail.com>
2025-04-18 18:25:39 -0400

    decent json serve done, TODO add classnames and better laytout in general for name, price, and description

--- scripts/json_serve.js
+++ scripts/json_serve.js
@@ -3,14 +3,30 @@ async function fetchMenu() {
     const request = new Request(requestURL);
 
     const response = await fetch(request);
-    const appetizers = await response.json();
-    serveAppetizers(appetizers)
-
+    return response.json();
 }
+async function serveCategory(divName, objCategory) { 
+    const obj = await fetchMenu();
+    const detailsSection = document.getElementById(divName);
+    
+    detailsSection.textContent = '';
 
-function serveAppetizers(obj) { 
-    const appetizerSection = document.getElementById("appetizer-div");
-    appetizerSection.textContent = obj.appetizers.name;
+    let name = document.createElement("div");
+    let description = document.createElement("div");
+    let price = document.createElement("div");
+
+    for (let i = 0; i < obj[objCategory].length; i++) {
+
+        const entry = document.createElement("li");
+        entry.classList.add("menu-entry");
+
+        name.textContent = obj[objCategory][i].name
+        description.textContent = obj[objCategory][i].description
+        price.textContent = obj[objCategory][i].price;
+        entry.appendChild(name);
+        entry.appendChild(description);
+        entry.appendChild(price);
+
+        detailsSection.appendChild(entry);
+    }
 }
-
-fetchMenu();
