7b9fdd7d81d3f5326504749b448dad456d817be6
landwhich <will.f.richards@gmail.com>
2025-04-18 23:25:54 -0400

    done json for appetizers and soup n salads, and fixed some styling and parsing for menu. TODO: layout for menu items and additional styling

--- scripts/json_serve.js
+++ scripts/json_serve.js
@@ -5,28 +5,50 @@ async function fetchMenu() {
     const response = await fetch(request);
     return response.json();
 }
-async function serveCategory(divName, objCategory) { 
+async function serveCategory(menuTag, objCategory) { 
     const obj = await fetchMenu();
-    const detailsSection = document.getElementById(divName);
-    
-    detailsSection.textContent = '';
-
-    let name = document.createElement("div");
-    let description = document.createElement("div");
-    let price = document.createElement("div");
+    const detailsSection = document.getElementById(menuTag);
 
     for (let i = 0; i < obj[objCategory].length; i++) {
 
         const entry = document.createElement("li");
         entry.classList.add("menu-entry");
 
+        let name = document.createElement("div");
+        let description = document.createElement("div");
+        let price = document.createElement("div");
+
         name.textContent = obj[objCategory][i].name
         description.textContent = obj[objCategory][i].description
+        if (Array.isArray(obj[objCategory][i].description)) {
+            description.textContent = ''
+            for (let j = 0; j < obj[objCategory][i].description.length; j++){
+                description.textContent += obj[objCategory][i].description[j] + '\n'
+            }
+            description.style.whiteSpace = 'pre-line';
+        }
         price.textContent = obj[objCategory][i].price;
+        if (Array.isArray(obj[objCategory][i].price)) {
+            price.textContent = ''
+            for (let j = 0; j < obj[objCategory][i].price.length; j++){
+                price.textContent += obj[objCategory][i].price[j] + '\n'
+            }
+            price.style.whiteSpace = 'pre-line';
+        }
+
         entry.appendChild(name);
         entry.appendChild(description);
         entry.appendChild(price);
 
         detailsSection.appendChild(entry);
     }
+}
+
+async function serveCategoryWithTitle(menuTag, titleField, objCategory){
+    const menu = document.getElementById(menuTag);
+    const title = document.createElement("h1");
+    title.innerText = titleField
+    
+    await serveCategory(menuTag, objCategory);
+    menu.appendChild(title);
 }
